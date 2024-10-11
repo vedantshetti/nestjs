46580d8ec88a97e2dcb10bfeac6d4d8d2c01e380
vedant shetti <vedantshetti123456@gmail.com>
2024-10-11 10:30:31 +0530

    learning nest js

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'; 
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],  // You can import other modules here
+})
+export class AppModule {}  // The root application module
